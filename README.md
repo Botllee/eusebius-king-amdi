cc4fedce358d278c04bed3baaf6253a27105e628
Botllee <131339523+Botllee@users.noreply.github.com>
2024-05-03 04:09:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Bottlee-Tech-session-generator
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+QR- WEB - PAIR CODE FOR BOT WITH WHISKEYSOCKETS/BAILEYS
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<p align="center">
+   <a href="https://github.com/Xcelsama">
+    <img src="" width="500">
+     
+</a>
+   <a aria-label="QRis free to use" href="https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X" target="_blank">
+ <p align="center"><img src="https://telegra.ph/file/cb0ca30c5924b296bd332.jpg" alt="xcelsama:: Visitor's Count" /></p>
+
+
+
+[`ℹ️Contact Owner`](https://wa.me/254710415721)
+
+FORK THE REPOSITORY (Repo) 
+    <br>
+<a href="https://github.com/Botllee/KING-AMDI-AI-pair/fork"><img title="WEB" src="https://img.shields.io/badge/FORK Gifted-QR?color=black&style=for-the-badge&logo=stackshare"></a>
+
+Now Deploy
+    <br>
+<a href='https://dashboard.heroku.com/new?template=https://github.com/Botllee/KING-AMDI-AI-pair' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=white'/>
+# `Owner`
+
+ <a href="https://github.com/Botllee"><img src="https://telegra.ph/file/cb0ca30c5924b296bd332.jpg" width="250" height="250" alt="Giftedmaurice"/></a>
+
+# 
+   
---
+++ qr.js
@@ -0,0 +1,105 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Keith_Tech,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function KING_AMDI_AI_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let KING_AMDI_AI_QR_CODE= Ibrahim_Tech({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_EUSEBIUS_TECH.ev.on('creds.update', saveCreds)
+			Qr_Code_By_EUSEBIUS_TECHr.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Ibrahim_King.sendMessage(Qr_Code_By_Ibrahim_King.user.id, { text: '' + b64data });
+	
+				   let KEITH_TECH_TEXT = `
+*_Qr Code By Keith tech_*
+*_Made With 🤖_*
+______________________________________
+╔════◇
+║ *『 KING-AMDI-AI 』*
+║ _YOU HAVE SUCCESSFULLY SELECTED THE BEST BOT._
+╚══════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _COMMING SOON
+║❒ *Owner:* __
+║❒ *Repo:* _
+║❒ *WaGroup:* _https://chat.whatsapp.com/KeEYrEX8UDdGD4m4GbdHaA_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47_
+║❒ *Plugins:* _https://github.com/Keithkeizzah/Keith-bot-md-plugins_
+╚══════════════════════╝ 
+_____________________________________
+	
+_Don't Forget To Give Star To My Repo_`
+	 await Qr_Code_EUSEBIUS_TECH.sendMessage(Qr_Code__EUSEBIUS_TECH.user.id,{text:EUSEBIUS_TECH_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code__EUSEBIUS_TECH.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					FLASH_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await IBRAHIM_TECH_QR_CODE()
+});
+module.exports = router
